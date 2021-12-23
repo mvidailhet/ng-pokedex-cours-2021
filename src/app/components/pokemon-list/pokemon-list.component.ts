90839b8a503737cc3629e5e775c71fc765f9c949
Michel Vidailhet <michel.vidailhet@gmail.com>
2021-12-23 15:51:30 +0100

    get pokemons from api

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -3,6 +3,11 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { PokemonService } from "src/app/services/pokemon.service";
 
+export interface Pokemon {
+  id?: string;
+  name: string;
+}
+
 @Component({
   selector: "app-pokemon-list",
   templateUrl: "./pokemon-list.component.html",
@@ -11,17 +16,22 @@ import { PokemonService } from "src/app/services/pokemon.service";
 export class PokemonListComponent implements OnInit {
   pokemonName = "";
   currentAddTimeout: any;
-  pokemons!: string[];
+  pokemons: Pokemon[] = [];
+
+  apiUrl =
+    "https://ng-pokedex-2021-mitch-default-rtdb.europe-west1.firebasedatabase.app";
 
   constructor(
     private pokemonService: PokemonService,
     private router: Router,
     private http: HttpClient
   ) {
-    this.pokemons = this.pokemonService.pokemons;
+    //this.pokemons = this.pokemonService.pokemons;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchPokemons();
+  }
 
   generateBackgroundColor() {
     return this.pokemons.length > 5 ? "#6200EE" : "#3700b3";
@@ -35,11 +45,23 @@ export class PokemonListComponent implements OnInit {
   }
 
   sendPokemonToApi(name: string) {
-    this.http.post(
-      "https://ng-pokedex-2021-mitch-default-rtdb.europe-west1.firebasedatabase.app/trucmuchs.json",
-      { name }
-    ).subscribe((responseData) => {
-      console.log(responseData);
+    this.http
+      .post(`${this.apiUrl}/pokemons.json`, { name })
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPokemons() {
+    this.http.get(`${this.apiUrl}/pokemons.json`).subscribe((pokemonsObject) => {
+      Object.entries(pokemonsObject).forEach(([key, pokemon]) => {
+        this.pokemons.push({
+          ...pokemon,
+          id: key,
+        });
+      });
+      console.log(pokemonsObject);
+      console.log(this.pokemons);
     });
   }
 
