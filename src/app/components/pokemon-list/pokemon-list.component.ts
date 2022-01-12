cdde51a5617aa0753b93486e4603d34c33d72e99
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-01-12 17:04:37 +0100

    show pokemons list

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { delay } from "rxjs";
 import { ApiService } from "src/app/services/api.service";
 import { Pokemon, PokemonService } from "src/app/services/pokemon.service";
 import { ToastService } from "src/app/services/toast.service";
@@ -14,6 +15,7 @@ export class PokemonListComponent implements OnInit {
   currentAddTimeout: any;
   pokemons: Pokemon[] = [];
   error: string | undefined;
+  isLoading = true;
 
   constructor(
     private pokemonService: PokemonService,
@@ -46,12 +48,15 @@ export class PokemonListComponent implements OnInit {
   }
 
   fetchPokemons() {
-    this.api.fetchPokemons().subscribe((pokemons: Pokemon[]) => {
+
+    this.api.fetchPokemons().pipe(delay(2000)).subscribe((pokemons: Pokemon[]) => {
       this.pokemons = [...pokemons];
       this.pokemonService.pokemons = this.pokemons;
+      this.isLoading = false;
     }, (error) => {
       //console.error(error);
       this.error = error.message; 
+      this.isLoading = false;
     });
   }
 }
